7f73ec37f3553d2a14d48a81256e6e8b93af0cd8
Kotarski <edktrsk@gmail.com>
2018-07-31 08:28:11 +0100

    fixed bug when SVGPoints used as vectors.

--- TS/_vector/-isCloseTo.ts
+++ TS/_vector/-isCloseTo.ts
@@ -1,6 +1,7 @@
 namespace _vector {
-   export function isCloseTo<T extends Vector>(inVector: T) {
+   export function isCloseTo(inVector: Vector) {
       return (compareVector: Vector, boundary: number = 5) => {
+
          const vectorXisClose = (Math.abs(inVector.x - compareVector.x) < boundary);
          const vectorYisClose = (Math.abs(inVector.y - compareVector.y) < boundary);
          return (vectorXisClose && vectorYisClose)
